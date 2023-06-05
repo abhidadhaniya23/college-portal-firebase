1800cef9a9c4ed797901f7f3921d3ed95cfc8bd1
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2023-06-05 08:22:08 +0530

    changed path of swUrl

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       console.log("swUrl: ", swUrl);
 
       if (isLocalhost) {
